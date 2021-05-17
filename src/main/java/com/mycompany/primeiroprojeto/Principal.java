f97faa02fb60169c7b3a999dfa9ec706f646275f
rodolfostokler <Stokler@DESKTOP-I8JIK2O>
2021-05-17 14:51:20 -0300

    Criado método main na classe principal

--- src/main/java/com/mycompany/primeiroprojeto/Principal.java
+++ src/main/java/com/mycompany/primeiroprojeto/Principal.java
@@ -13,6 +13,6 @@ public class Principal {
     //Método principal
     public static void main(String[] args) {
         System.out.println("Olá mundo");
-        
+        System.out.println("Olá Rodolfo");
     }
 }
