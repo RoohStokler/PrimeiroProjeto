0e0734ea57fced942a450d02c2d05344ffe21d46
RoohStokler <84341888+RoohStokler@users.noreply.github.com>
2021-05-17 15:24:39 -0300

    vem

--- src/main/java/com/mycompany/primeiroprojeto/Principal.java
+++ src/main/java/com/mycompany/primeiroprojeto/Principal.java
@@ -15,5 +15,6 @@ public class Principal {
         System.out.println("Olá mundo");
         System.out.println("Olá Rodolfo");
         System.out.println("Como vai");
+        System.out.println("Vem tranquilo");
     }
 }
